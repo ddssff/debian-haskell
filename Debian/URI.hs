efe99527d69d69cf098f0d1d4556e50245681605
David Fox <dsf@seereason.com>
2012-03-27 00:07:09 +0000

    Eliminate dependency on progress package

--- Debian/URI.hs
+++ Debian/URI.hs
@@ -14,8 +14,11 @@ import qualified Data.ByteString as B
 import Data.Maybe (catMaybes)
 import Network.URI
 import System.Directory (getDirectoryContents)
-import System.Unix.LazyProcess (collectStdout)
-import System.Unix.Progress (lazyCommandF)
+import System.Exit (ExitCode(..))
+import System.Process (showCommandForUser)
+import System.Unix.Process (readProcessWithExitCode)
+-- import System.Unix.LazyProcess (collectStdout)
+-- import System.Unix.Progress (lazyCommandF)
 import Text.Regex (mkRegex, matchRegex)
 
 uriToString' :: URI -> String
@@ -28,25 +31,38 @@ instance Ord URI where
 type URIString = String
 
 fileFromURI :: URI -> IO (Either SomeException L.ByteString)
-fileFromURI uri =
+fileFromURI uri = try $
     case (uriScheme uri, uriAuthority uri) of
-      ("file:", Nothing) -> try (L.readFile (uriPath uri))
+      ("file:", Nothing) -> L.readFile (uriPath uri)
       -- ("ssh:", Just auth) -> cmdOutput ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " cat " ++ show (uriPath uri))
-      ("ssh:", Just auth) -> try (lazyCommandF ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " cat " ++ show (uriPath uri)) L.empty >>=
-                                  return . fst . collectStdout)
-      _ -> try (lazyCommandF ("curl -s -g '" ++ uriToString' uri ++ "'") L.empty >>=
-                return . fst . collectStdout)
+      ("ssh:", Just auth) ->
+          do let cmd  = "ssh"
+                 args = [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "cat", uriPath uri]
+             (code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+             case code of
+               ExitFailure _ -> error (showCommandForUser cmd args ++ " -> " ++ show code)
+               _ -> return out
+      _ ->
+          do let cmd = "curl"
+                 args = ["-s", "-g", uriToString' uri]
+             (_code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+             return out
 
 fileFromURIStrict :: URI -> IO (Either SomeException B.ByteString)
-fileFromURIStrict uri =
+fileFromURIStrict uri = try $
     case (uriScheme uri, uriAuthority uri) of
-      ("file:", Nothing) -> try (B.readFile (uriPath uri))
+      ("file:", Nothing) -> B.readFile (uriPath uri)
       -- ("ssh:", Just auth) -> cmdOutputStrict ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " cat " ++ show (uriPath uri))
-      ("ssh:", Just auth) -> try (lazyCommandF ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " cat " ++ show (uriPath uri)) L.empty >>=
-                                  return . B.concat . L.toChunks . fst . collectStdout)
-      _ -> try (lazyCommandF ("curl -s -g '" ++ uriToString' uri ++ "'") L.empty >>=
-                return . B.concat . L.toChunks . fst . collectStdout)
-      -- _ -> cmdOutputStrict ("curl -s -g '" ++ uriToString' uri ++ "'")
+      ("ssh:", Just auth) -> do
+          let cmd = "ssh"
+              args = [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "cat", uriPath uri]
+          (_code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+          return . B.concat . L.toChunks $ out
+      _ -> do
+          let cmd = "curl"
+              args = ["-s", "-g", uriToString' uri]
+          (_code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+          return . B.concat . L.toChunks $ out
 
 -- | Parse the text returned when a directory is listed by a web
 -- server.  This is currently only known to work with Apache.
@@ -62,11 +78,16 @@ webServerDirectoryContents text =
 
 
 dirFromURI :: URI -> IO (Either SomeException [String])
-dirFromURI uri =
+dirFromURI uri = try $
     case (uriScheme uri, uriAuthority uri) of
-      ("file:", Nothing) -> try (getDirectoryContents (uriPath uri))
-      -- ("ssh:", Just auth) -> cmdOutput ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " ls -1 " ++ uriPath uri) >>=
-      ("ssh:", Just auth) -> try (lazyCommandF ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " ls -1 " ++ uriPath uri) L.empty >>=
-                                  return . lines . L.unpack . fst . collectStdout)
-      -- _ -> cmdOutput ("curl -s -g '" ++ uriToString' uri ++ "/'") >>= return . either Left (Right . webServerDirectoryContents)
-      _ -> try (lazyCommandF ("curl -s -g '" ++ uriToString' uri ++ "/'") L.empty >>= return . webServerDirectoryContents . fst . collectStdout)
+      ("file:", Nothing) -> getDirectoryContents (uriPath uri)
+      ("ssh:", Just auth) ->
+          do let cmd = "ssh"
+                 args = [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "ls", "-1", uriPath uri]
+             (_code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+             return . lines . L.unpack $ out
+      _ ->
+          do let cmd = "curl"
+                 args = ["-s", "-g", uriToString' uri]
+             (_code, out, _err) <- readProcessWithExitCode cmd args id L.empty
+             return . webServerDirectoryContents $ out
