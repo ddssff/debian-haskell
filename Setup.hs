b87872fac1ec4817ac7e79bd04e946e0063140d5
David Fox <dsf@seereason.com>
2015-03-02 12:26:51 -0800

    Remove old code to run what is now a test suite

--- Setup.hs
+++ Setup.hs
@@ -8,12 +8,4 @@ import System.Exit
 import System.FilePath ((</>))
 import System.Process
 
-main = copyFile "debian/changelog" "changelog" >>
-       defaultMainWithHooks simpleUserHooks {
-         postBuild = \ _ _ _ lbi -> runTestScript lbi
-       , runTests = \ _ _ _ lbi -> runTestScript lbi
-       }
-
-runTestScript lbi =
-    system (buildDir lbi </> "debian-tests/debian-tests") >>= \ code ->
-    if code == ExitSuccess then return () else error "Test Failure"
+main = copyFile "debian/changelog" "changelog" >> defaultMainWithHooks simpleUserHooks
