2619edd9c34c2808b78289506b740908f0a19b49
David Fox <dsf@seereason.com>
2016-10-06 10:26:11 -0700

    Make changelog a duplicate of debian/changelog

--- Setup.hs
+++ Setup.hs
@@ -1,11 +1,5 @@
 #!/usr/bin/runhaskell
 
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
-import Distribution.Simple.Program
-import System.Directory
-import System.Exit
-import System.FilePath ((</>))
-import System.Process
 
-main = copyFile "debian/changelog" "changelog" >> defaultMainWithHooks simpleUserHooks
+main = defaultMainWithHooks simpleUserHooks
