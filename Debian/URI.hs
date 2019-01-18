7b98582241d15a04676d07895f72f1bc95ddaf15
David Fox <dsf@seereason.com>
2019-01-18 06:26:18 -0800

    Version 3.93.4 - better handling of shell command errors

--- Debian/URI.hs
+++ Debian/URI.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE PackageImports #-}
-{-# OPTIONS -fno-warn-orphans #-}
+{-# OPTIONS -Wall -fno-warn-orphans #-}
 module Debian.URI
     ( module Network.URI
     , URI'
@@ -12,14 +12,19 @@ module Debian.URI
     , dirFromURI
     ) where
 
-import Control.Exception (SomeException, try)
+import Control.Exception (SomeException, throw, try)
 import Data.ByteString.Lazy.UTF8 as L
 import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Maybe (catMaybes, fromJust)
+import Data.Text as T (unpack)
+import Data.Text.Encoding (decodeUtf8)
 import Network.URI (URI(..), URIAuth(..), parseURI, uriToString)
 import System.Directory (getDirectoryContents)
+import System.Exit (ExitCode(..))
 -- import System.Process.ByteString (readProcessWithExitCode)
-import System.Process.ByteString.Lazy (readProcessWithExitCode)
+import System.Process (CreateProcess, proc)
+import System.Process.Common (showCreateProcessForUser)
+import System.Process.ByteString.Lazy (readCreateProcessWithExitCode)
 import Text.Regex (mkRegex, matchRegex)
 
 -- | A wrapper around a String containing a known parsable URI.  Not
@@ -50,16 +55,10 @@ fileFromURIStrict uri = try $
     case (uriScheme uri, uriAuthority uri) of
       ("file:", Nothing) -> L.readFile (uriPath uri)
       -- ("ssh:", Just auth) -> cmdOutputStrict ("ssh " ++ uriUserInfo auth ++ uriRegName auth ++ uriPort auth ++ " cat " ++ show (uriPath uri))
-      ("ssh:", Just auth) -> do
-          let cmd = "ssh"
-              args = [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "cat", uriPath uri]
-          (_code, out, _err) <- readProcessWithExitCode cmd args L.empty
-          return out
-      _ -> do
-          let cmd = "curl"
-              args = ["-s", "-g", uriToString' uri]
-          (_code, out, _err) <- readProcessWithExitCode cmd args L.empty
-          return out
+      ("ssh:", Just auth) ->
+          run (proc "ssh" [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "cat", uriPath uri])
+      _ ->
+          run (proc "curl" ["-s", "-g", uriToString' uri])
 
 -- | Parse the text returned when a directory is listed by a web
 -- server.  This is currently only known to work with Apache.
@@ -79,12 +78,18 @@ dirFromURI uri = try $
     case (uriScheme uri, uriAuthority uri) of
       ("file:", Nothing) -> getDirectoryContents (uriPath uri)
       ("ssh:", Just auth) ->
-          do let cmd = "ssh"
-                 args = [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "ls", "-1", uriPath uri]
-             (_code, out, _err) <- readProcessWithExitCode cmd args L.empty
-             return . Prelude.lines . L.toString $ out
+          (Prelude.lines . L.toString) <$>
+            run (proc "ssh" [uriUserInfo auth ++ uriRegName auth ++ uriPort auth, "ls", "-1", uriPath uri])
       _ ->
-          do let cmd = "curl"
-                 args = ["-s", "-g", uriToString' uri]
-             (_code, out, _err) <- readProcessWithExitCode cmd args L.empty
-             return . webServerDirectoryContents $ out
+          webServerDirectoryContents <$>
+            run (proc "curl" ["-s", "-g", uriToString' uri])
+
+run :: CreateProcess -> IO L.ByteString
+run cp = do
+  (code, out, err) <- readCreateProcessWithExitCode cp L.empty
+  case code of
+    ExitSuccess -> return out
+    ExitFailure _ -> throw (userError (show code ++ "\n" ++
+                                       " command: " ++ showCreateProcessForUser cp ++ "\n" ++
+                                       " stderr: " ++ unpack (decodeUtf8 (L.toStrict err)) ++ "\n" ++
+                                       " stdout: " ++ unpack (decodeUtf8 (L.toStrict out))))
